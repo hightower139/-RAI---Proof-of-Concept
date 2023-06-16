5e1a51d0309876ef5a9fd5d72d6aa869f2e5da0a
hightower139 <john.a.talbert3@gmail.com>
2023-06-16 18:10:38 -0500

    I have made a peofile system that is not encrypted yet, but it does work as a proof of concept, there is gonna be ways to break the file but for now it is working.

--- RAI_Main.py
+++ RAI_Main.py
@@ -5,34 +5,33 @@ import os
 
 from paramiko import SSHClient
 from tkinter import messagebox
-from tkinter import simpledialog
 
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(par.AutoAddPolicy())
 
 
 #Creates a Login popup
-class login:
+class login_server:
 
     def __init__(self):
-        self.login = tk.Tk()
-        self.login.title("Login")
-        self.login.iconbitmap("myIcon.ico")
+        self.login_server = tk.Tk()
+        self.login_server.title("Login")
+        self.login_server.iconbitmap("myIcon.ico")
 
-        self.login_label = tk.Label(self.login, text="Login Information", font=('Arial', 18))
+        self.login_server_label = tk.Label(self.login_server, text=" Server Login Information", font=('Arial', 18))
 
-        self.hostname_label = tk.Label(self.login, text="Hostname: ", font=('Arial', 12))
-        self.hostname_box = tk.Entry(self.login, font=('Arial', 12))
+        self.hostname_label = tk.Label(self.login_server, text="Hostname: ", font=('Arial', 12))
+        self.hostname_box = tk.Entry(self.login_server, font=('Arial', 12))
 
-        self.username_label = tk.Label(self.login, text="Username: ", font=('Arial', 12))
-        self.username_box = tk.Entry(self.login, font=('Arial', 12))
+        self.username_label = tk.Label(self.login_server, text="Username: ", font=('Arial', 12))
+        self.username_box = tk.Entry(self.login_server, font=('Arial', 12))
 
-        self.password_label = tk.Label(self.login, text="Password: ", font=('Arial', 12))
-        self.password_box = tk.Entry(self.login, font=('Arial', 12), show='*')
+        self.password_label = tk.Label(self.login_server, text="Password: ", font=('Arial', 12))
+        self.password_box = tk.Entry(self.login_server, font=('Arial', 12), show='*')
 
-        self.login_button = tk.Button(self.login, text='login', font=('Arial', 12), command=self.try_login)
+        self.login_server_button = tk.Button(self.login_server, text='login', font=('Arial', 12), command=self.try_login)
 
-        self.login_label.grid(row=0, columnspan=2)
+        self.login_server_label.grid(row=0, columnspan=2)
 
         self.hostname_label.grid(row=1, column=0)
         self.hostname_box.grid(row=1, column=1)
@@ -43,24 +42,30 @@ class login:
         self.password_label.grid(row=3, column=0)
         self.password_box.grid(row=3, column=1)
 
-        self.login_button.grid(row=4, columnspan=2)
+        self.login_server_button.grid(row=4, columnspan=2)
 
-        self.login.mainloop()
+        self.login_server.mainloop()
+
+        self.close()
 
     #Logs you into the server
     def try_login(self):
-
         ip = self.hostname_box.get()
         try_ip = os.system('ping %s -n 1' % (ip,))
         if try_ip == 0:
-            print("Pingable")
             user = self.username_box.get()
             secret = self.password_box.get()
             ssh.connect(ip, username=user, password=secret)
             ssh.exec_command('wall "Hello World"')
             ssh.close()
+            self.login_server.destroy()
+            self.close()
         else:
-            messagebox.showinfo(title = "Error", message = "Could not not reach host please make sure that the host is reachable or that the hostname is correct")
+            messagebox.showinfo(title = "Error", message = "Could not reach host please make sure that the host is reachable or that the hostname is correct")
+    
+    def close(self):
+        MyGUI()
+        self.login_server.destroy()
         
 
 #Main GUI front page
@@ -70,25 +75,26 @@ class MyGUI:
         self.root = tk.Tk()
         self.root.title("RAI - Proof of Concept")
         self.root.iconbitmap("myIcon.ico")
+        self.root.configure(bg="#404040")
 
-        self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18))
+        self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18), bg='#404040', fg='#ffffff')
         self.label.pack(padx=10, pady=10)
 
-        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
+        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16), bg='#404040', fg='#ffffff')
         self.textbox.pack(padx=10, pady=10)
 
         self.check_state = tk.IntVar()
 
-        self.checkbox = tk.Checkbutton(self.root, text='Show Message Box', font=('Arial', 16), variable=self.check_state)
+        self.checkbox = tk.Checkbutton(self.root, text='Show Message Box', font=('Arial', 16), bg='#404040', fg='#ffffff',  variable=self.check_state)
         self.checkbox.pack(padx=10, pady=10)
 
-        self.button = tk.Button(self.root, text='Show Message', font=('Arial', 18), command=self.show_message)
+        self.button = tk.Button(self.root, text='Show Message', font=('Arial', 18), bg='#404040', fg='#ffffff', command=self.show_message)
         self.button.pack(padx=10, pady=10)
 
-        launch_chrome = tk.Button(self.root, text="Launch Chrome", font=("Arial", 18), command=self.chrome)
+        launch_chrome = tk.Button(self.root, text="Launch Chrome", font=("Arial", 18), bg='#404040', fg='#ffffff', command=self.chrome)
         launch_chrome.pack(padx=10, pady=10)
 
-        launch_dawncraft = tk.Button(self.root, text="Launch DawnCraft", font=("Arial", 18), command=self.dawncraft)
+        launch_dawncraft = tk.Button(self.root, text="Launch DawnCraft", font=("Arial", 18), bg='#404040', fg='#ffffff', command=self.dawncraft)
         launch_dawncraft.pack(padx=10, pady=10)
 
         self.root.mainloop()
@@ -103,6 +109,83 @@ class MyGUI:
         subprocess.Popen("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
 
     def dawncraft(self):
-        login()
+        self.root.destroy()
+        login_server()
 
-MyGUI()
+# This is the class to create a profile login screen
+class profile_login():
+
+    def __init__(self):
+
+        self.directory = os.listdir("profiles")
+        
+        self.profile_login = tk.Tk()
+        self.profile_login.title("Login")
+        self.profile_login.iconbitmap("myIcon.ico")
+        self.profile_login.resizable(False, False)
+        self.profile_login_label = tk.Label(self.profile_login, text="Profile Login Information", font=('Arial', 18))
+
+        self.username_label = tk.Label(self.profile_login, text="Username: ", font=('Arial', 12))
+        self.username_box = tk.Entry(self.profile_login, font=('Arial', 12))
+
+        self.password_label = tk.Label(self.profile_login, text="Password: ", font=('Arial', 12))
+        self.password_box = tk.Entry(self.profile_login, font=('Arial', 12), show='*')
+
+        self.profile_login_button = tk.Button(self.profile_login, text='login', font=('Arial', 12), command=self.try_login_profile)
+        self.profile_create_button = tk.Button(self.profile_login, text='Create Profile', font=('Arial', 12), command=self.create_profile)
+
+        self.profile_login_label.grid(row=0, columnspan=2)
+        
+        self.username_label.grid(row=1, column=0)
+        self.username_box.grid(row=1, column=1)
+
+        self.password_label.grid(row=2, column=0)
+        self.password_box.grid(row=2, column=1)
+
+        if len(self.directory) == 0:
+            self.profile_create_button.grid(row=3, columnspan=2, sticky="ew")
+        else:
+            self.profile_login_button.grid(row=3, column=0, sticky="ew")
+            self.profile_create_button.grid(row=3, column=1, sticky="ew")
+
+        self.profile_login.mainloop()
+
+    def create_profile(self):
+        self.user = self.username_box.get()
+        self.secret = self.password_box.get()
+        self.profile_directory = "profiles\\" + self.user + ".txt"
+        print(self.user)
+        print(self.secret)
+        
+        with open(self.profile_directory, "w") as f:
+            f.write(str(self.secret))
+            f.close()
+        
+        MyGUI()
+        self.profile_login.destroy()
+
+    # Logs you into you're profile
+    def try_login_profile(self):
+        self.user = self.username_box.get()
+        self.secret = self.password_box.get()
+        self.secret
+        self.profile_directory = "profiles\\" + self.user + ".txt"
+
+        if os.path.isfile(self.profile_directory):
+            print("made it")
+            with open(self.profile_directory, "r") as f:
+                first_line = f.readline().strip()
+                print(f"{first_line} == {self.secret}")
+                if first_line == self.secret:
+                    MyGUI()
+                    self.profile_login.destroy()
+                else:
+                    messagebox.showinfo(title = "Can not Login", message = "Could not login check username and password")
+                f.close()
+
+        else:
+            messagebox.showinfo(title = "Can not Login", message = "Could not login check username and password")
+
+#MyGUI()
+
+profile_login()
